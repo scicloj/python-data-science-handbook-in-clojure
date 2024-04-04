1d9338908725d3086a813ee11dae2c26c6f1959d
daslu <daniel.slutsky@gmail.com>
2024-04-04 23:54:18 +0300

    basic setup

---
+++ src/scicloj/handbook/util.clj
@@ -0,0 +1,9 @@
+(ns scicloj.handbook.util
+  (:require [scicloj.kindly.v4.kind :as kind]
+            [scicloj.kindly.v4.api :as kindly]))
+
+(defn quote-python [code]
+  (->> code
+       (format "\nPython:\n```%s```")
+       kind/md
+       kindly/hide-code))
