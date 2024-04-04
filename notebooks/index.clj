7c107ff0293cab8b40aaa9aa136cc463851d6904
daslu <daniel.slutsky@gmail.com>
2024-04-04 23:56:00 +0300

    book setup

---
+++ notebooks/index.clj
@@ -0,0 +1,6 @@
+^:kindly/hide-code
+(ns index)
+
+;; # Preface
+
+;; These notes reproduce parts of the [code](https://github.com/jakevdp/PythonDataScienceHandbook) of the [Python Data Science Notebook](https://jakevdp.github.io/PythonDataScienceHandbook/) by Jake VanderPlas.
